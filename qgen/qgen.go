e97342f91469d7bc7d23ee64e2c7ca454ae0cf87
gary <mcsd2001@163.com>
2019-04-10 02:11:12 +0800

    0.2

--- qgen/qgen.go
+++ qgen/qgen.go
@@ -38,7 +38,8 @@ func cm(x int) string {
 	return strconv.Itoa(x)
 }
 
-func genProblems(level int, sign bool, problemCount int) (problems []string, answers []string, seed int64) {
+// Gernate Factorization Problems
+func genFactProblems(level int, sign bool, problemCount int) (problems []string, answers []string, seed int64) {
 
 	// problems = make([]string, problem_count)
 	// answers = make([]string, problem_count)
@@ -61,30 +62,30 @@ func genProblems(level int, sign bool, problemCount int) (problems []string, ans
 	seed = time.Now().UnixNano()
 	rand.Seed(seed)
 
-	var pvalues []int
+	var possibleValues []int
 	if sign {
 		for i := -5 * level; i <= 5*level; i++ {
 			if i == 0 {
 				continue
 			}
-			pvalues = append(pvalues, i)
+			possibleValues = append(possibleValues, i)
 		}
 	} else {
 		for i := 1; i <= 5*level; i++ {
 			if i == 0 {
 				continue
 			}
-			pvalues = append(pvalues, i)
+			possibleValues = append(possibleValues, i)
 		}
 	}
 
-	length := len(pvalues)
+	length := len(possibleValues)
 	for i := 0; i < problemCount; i++ {
 
-		a := pvalues[rand.Intn(length)]
-		b := pvalues[rand.Intn(length)]
-		c := pvalues[rand.Intn(length)]
-		d := pvalues[rand.Intn(length)]
+		a := possibleValues[rand.Intn(length)]
+		b := possibleValues[rand.Intn(length)]
+		c := possibleValues[rand.Intn(length)]
+		d := possibleValues[rand.Intn(length)]
 
 		// problems[i] = fmt.Sprintf("%sx^2+%sx+%d=", xi(a*b), xim(a*d+b*c), c*d)
 		// answers[i] = fmt.Sprintf("%sx^2+%sx+%d=(%sx+%d)*(%sx+%d)", xi(a*b), xim(a*d+b*c), c*d, xi(a), c, xi(b), d)
@@ -94,49 +95,81 @@ func genProblems(level int, sign bool, problemCount int) (problems []string, ans
 	return
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+// Gernate Linear Euqation with 2 unknowns Problems
+func genLe2uProblems(level int, sign bool, problemCount int) (problems []string, answers []string, seed int64) {
 
-	tplHTMLData, err1 := ioutil.ReadFile("template.html")
-	var newHtml string = ""
-	if err1 == nil {
-		html := string(tplHTMLData)
-		// fmt.Println(html)
+	// problems = make([]string, problem_count)
+	// answers = make([]string, problem_count)
 
-		var problemsHTML, answersHTML string = "", ""
-		problems, answers, _ := genProblems(1, false, 15)
-		for index := 0; index < len(problems); index++ {
-			// fmt.Println(problems[index])
-			problemsHTML += fmt.Sprintf("$$%s$$\n", problems[index])
-			answersHTML += fmt.Sprintf("$$%s$$\n", answers[index])
-		}
+	// seed = time.Now().UnixNano()
+	// rand.Seed(seed)
 
-		// fmt.Println(problemsHTML)
-		// fmt.Println(answersHTML)
-		newHtml = strings.Replace(html, "#PROBLEMS#", problemsHTML, -1)
-		newHtml = strings.Replace(newHtml, "#ANSWERS#", answersHTML, -1)
-		// fmt.Println(newHtml)
-	}
-
-	// write to disk
-	// problemHtmlData := []byte(newHtml)
-	// err2 := ioutil.WriteFile("problems.html", problemHtmlData, 0644)
-	// if err2 == nil {
-	// 	fmt.Println("Done.")
+	// for i := 0; i < problem_count; i++ {
+	// 	a := rand.Intn(level) + 1
+	// 	b := rand.Intn(level) + 1
+	// 	c := rand.Intn(level) + 1
+	// 	d := rand.Intn(level) + 1
+	// 	problems[i] = fmt.Sprintf("%dx^2+%dx+%d=", a*b, a*d+b*c, c*d)
+	// 	answers[i] = fmt.Sprintf("%dx^2+%dx+%d=(%dx+%d)*(%dx+%d)", a*b, a*d+b*c, c*d, a, c, b, d)
 	// }
 
-	fmt.Fprintf(w, newHtml)
+	problems = make([]string, problemCount)
+	answers = make([]string, problemCount)
+
+	seed = time.Now().UnixNano()
+	rand.Seed(seed)
+
+	var possibleValues []int
+	if sign {
+		for i := -5 * level; i <= 5*level; i++ {
+			if i == 0 {
+				continue
+			}
+			possibleValues = append(possibleValues, i)
+		}
+	} else {
+		for i := 1; i <= 5*level; i++ {
+			if i == 0 {
+				continue
+			}
+			possibleValues = append(possibleValues, i)
+		}
+	}
+
+	length := len(possibleValues)
+	for i := 0; i < problemCount; i++ {
+
+		a := possibleValues[rand.Intn(length)]
+		b := possibleValues[rand.Intn(length)]
+		c := possibleValues[rand.Intn(length)]
+		d := possibleValues[rand.Intn(length)]
+
+		x := possibleValues[rand.Intn(length)]
+		y := possibleValues[rand.Intn(length)]
+		// problems[i] = fmt.Sprintf("%sx^2+%sx+%d=", xi(a*b), xim(a*d+b*c), c*d)
+		// answers[i] = fmt.Sprintf("%sx^2+%sx+%d=(%sx+%d)*(%sx+%d)", xi(a*b), xim(a*d+b*c), c*d, xi(a), c, xi(b), d)
+		problems[i] = fmt.Sprintf("%sx%sy=%d \\\\ %sx%sy=%d", xi(a), xis(b), a*x+b*y, xi(c), xis(d), c*x+d*y)
+		answers[i] = fmt.Sprintf("x=%d \\\\ y=%d ", x, y)
+	}
+	return
 }
 
-func coolHandler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tplHTMLData, _ := ioutil.ReadFile("index.tmpl.html")
+	contentHTML := string(tplHTMLData)
+	fmt.Fprintf(w, contentHTML)
+}
 
-	tplHTMLData, err1 := ioutil.ReadFile("template.html")
-	var newHtml string = ""
+func factHandler(w http.ResponseWriter, r *http.Request) {
+
+	tplHTMLData, err1 := ioutil.ReadFile("problems.tmpl.html")
+	var newHTML string
 	if err1 == nil {
 		html := string(tplHTMLData)
 		// fmt.Println(html)
 
 		var problemsHTML, answersHTML string = "", ""
-		problems, answers, _ := genProblems(1, true, 3)
+		problems, answers, _ := genFactProblems(1, false, 5)
 		for index := 0; index < len(problems); index++ {
 			// fmt.Println(problems[index])
 			problemsHTML += fmt.Sprintf("$$%s$$\n", problems[index])
@@ -145,25 +178,60 @@ func coolHandler(w http.ResponseWriter, r *http.Request) {
 
 		// fmt.Println(problemsHTML)
 		// fmt.Println(answersHTML)
-		newHtml = strings.Replace(html, "#PROBLEMS#", problemsHTML, -1)
-		newHtml = strings.Replace(newHtml, "#ANSWERS#", answersHTML, -1)
-		// fmt.Println(newHtml)
+		newHTML = strings.Replace(html, "#PROBLEMS#", problemsHTML, -1)
+		newHTML = strings.Replace(newHTML, "#ANSWERS#", answersHTML, -1)
+		// fmt.Println(newHTML)
 	}
 
 	// write to disk
-	// problemHtmlData := []byte(newHtml)
+	// problemHtmlData := []byte(newHTML)
 	// err2 := ioutil.WriteFile("problems.html", problemHtmlData, 0644)
 	// if err2 == nil {
 	// 	fmt.Println("Done.")
 	// }
 
-	fmt.Fprintf(w, newHtml)
+	fmt.Fprintf(w, newHTML)
+}
+
+func le2uHandler(w http.ResponseWriter, r *http.Request) {
+
+	tplHTMLData, err1 := ioutil.ReadFile("problems.tmpl.html")
+	var newHTML string
+	if err1 == nil {
+		html := string(tplHTMLData)
+		// fmt.Println(html)
+
+		var problemsHTML, answersHTML string = "", ""
+		problems, answers, _ := genLe2uProblems(1, false, 5)
+		// problems, answers, _ := genLe2uProblems(1, true, 5)
+		for index := 0; index < len(problems); index++ {
+			// fmt.Println(problems[index])
+			problemsHTML += fmt.Sprintf("$$%d. \\begin{cases}%s\\end{cases}$$\n", index+1, problems[index])
+			answersHTML += fmt.Sprintf("$$%d. \\begin{cases}%s\\end{cases}$$\n", index+1, answers[index])
+		}
+
+		// fmt.Println(problemsHTML)
+		// fmt.Println(answersHTML)
+		newHTML = strings.Replace(html, "#PROBLEMS#", problemsHTML, -1)
+		newHTML = strings.Replace(newHTML, "#ANSWERS#", answersHTML, -1)
+		// fmt.Println(newHTML)
+	}
+
+	// write to disk
+	// problemHtmlData := []byte(newHTML)
+	// err2 := ioutil.WriteFile("problems.html", problemHtmlData, 0644)
+	// if err2 == nil {
+	// 	fmt.Println("Done.")
+	// }
+
+	fmt.Fprintf(w, newHTML)
 }
 
 func main() {
 
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/cool", coolHandler)
+	http.HandleFunc("/fact", factHandler)
+	http.HandleFunc("/le2u", le2uHandler)
 	http.ListenAndServe(":9923", nil)
 
 }
